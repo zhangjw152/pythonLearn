b01cd254d6679390527fb1b8fe18a53029bcf10d
junwenzhang <junwenzhang@sohu_inc.com>
2016-12-20 18:48:07 +0800

    1220

---
+++ eg7.2.py
@@ -0,0 +1,11 @@
+def twoMax(a, b):
+    if a > b:
+        print a, 'is max'
+    else:
+        print b, 'is max'
+
+
+twoMax(2, 67)
+a = input('the first one')
+b = input('the second')
+twoMax(a, b)
